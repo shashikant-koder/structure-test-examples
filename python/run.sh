7c8423e0d78b3f893b9373c417d480bd1d18ad8b
kants3 <shashi.kant@in.pega.com>
2019-06-06 17:45:58 +0500

    modified run.sh

--- python/run.sh
+++ python/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 structure-test -test.v -image gcr.io/google-appengine/python \
-python_command_tests.yaml
-
-structure-test -test.v -image gcr.io/google-appengine/python \
 python_file_tests.yaml
